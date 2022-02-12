7d421e456cdc09c1334ec0d9b8b90fe0ea3daf73
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-02-12 22:52:36 +0900

    CS

--- lib/src/ui/widgets/login_page.dart
+++ lib/src/ui/widgets/login_page.dart
@@ -92,7 +92,7 @@ class _LoginPageState extends State<LoginPage> {
               GestureDetector(
                 onTap: () {
                   BlocProvider.of<LoginBloc>(context)
-                      .add(LoginClickOnRegisterEvent());
+                      .add(const LoginClickOnRegisterEvent());
                 },
                 child: Center(
                   child: Text(
