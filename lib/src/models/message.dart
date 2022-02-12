badf68f0fb558ae97cfa040effd0140efdd8fb2e
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-02-12 17:55:00 +0900

    Add message data model

---
+++ lib/src/models/message.dart
@@ -0,0 +1,28 @@
+class Message {
+  int id;
+  DateTime createdAt;
+  DateTime updatedAt;
+  int senderId;
+  int chatId;
+  String content;
+
+  Message({
+    required this.id,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.senderId,
+    required this.chatId,
+    required this.content,
+  });
+
+  factory Message.fromJson(Map<String, dynamic> parsedJson) {
+    return Message(
+      id: parsedJson["id"],
+      createdAt: parsedJson["created_at"],
+      updatedAt: parsedJson["updated_at"],
+      senderId: parsedJson["sender_id"],
+      chatId: parsedJson["chat_id"],
+      content: parsedJson["content"],
+    );
+  }
+}
