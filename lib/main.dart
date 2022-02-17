f845442f3f1439693f76d5041c9267d93fdcb2ce
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-02-17 18:17:35 +0900

    Remove useless future builder

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:organia/src/blocs/login/bloc.dart';
 import 'package:organia/src/ui/routes/main_routes.dart';
-import 'package:organia/src/utils/shared_preferences.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,16 +10,6 @@ void main() {
       return LoginBloc();
     }),
   ], child: const App()));
-}
-
-Future<bool> _loadCredentials() async {
-  final auth = await MySharedPreferences().get("AUTH");
-
-  if (auth == null) {
-    return (false);
-  }
-
-  return (true);
 }
 
 class App extends StatefulWidget {
@@ -33,30 +22,19 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _loadCredentials(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
-            builder: (context, child) => MediaQuery(
-                data: MediaQuery.of(context)
-                    .copyWith(alwaysUse24HourFormat: true),
-                child: child!),
-            initialRoute: (snapshot.data == true ? '/home' : '/'),
-            title: 'OrganIA',
-            theme: ThemeData(
-              brightness: Brightness.light,
-            ),
-            darkTheme: ThemeData(
-              brightness: Brightness.dark,
-            ),
-            home: const MainRoutes(),
-          );
-        } else {
-          return const MaterialApp(
-              home: Center(child: CircularProgressIndicator()));
-        }
-      },
+    return MaterialApp(
+      builder: (context, child) => MediaQuery(
+        data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+        child: child!,
+      ),
+      title: 'OrganIA',
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const MainRoutes(),
     );
   }
 }
