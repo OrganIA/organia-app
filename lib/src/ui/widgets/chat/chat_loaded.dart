1f3803136300a51cac930f7cda1f0f750e4b9408
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-03-17 12:50:16 +0900

    Fix ui

--- lib/src/ui/widgets/chat/chat_loaded.dart
+++ lib/src/ui/widgets/chat/chat_loaded.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:organia/src/models/chat.dart';
 import 'package:organia/src/models/message.dart';
@@ -21,8 +23,14 @@ class ChatLoadedPage extends StatefulWidget {
 }
 
 class _ChatLoadedPageState extends State<ChatLoadedPage> {
+  final ScrollController controller = ScrollController();
   @override
   Widget build(BuildContext context) {
+    SchedulerBinding.instance?.addPostFrameCallback((_) {
+      controller.jumpTo(
+        controller.position.maxScrollExtent,
+      );
+    });
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -85,99 +93,107 @@ class _ChatLoadedPageState extends State<ChatLoadedPage> {
           ),
         ),
       ),
-      body: Stack(
-        children: <Widget>[
-          ListView.builder(
-            itemCount: widget.messages.length,
-            shrinkWrap: true,
-            padding: const EdgeInsets.only(top: 20, bottom: 20),
-            // physics: const NeverScrollableScrollPhysics(),
-            itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.only(
-                    left: 14, right: 14, top: 10, bottom: 10),
-                child: Align(
-                  alignment: (widget.messages[index].senderId != widget.userId
-                      ? Alignment.topLeft
-                      : Alignment.topRight),
-                  child: Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color:
-                              (widget.messages[index].senderId != widget.userId
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).size.height -
+                  AppBar().preferredSize.height -
+                  84,
+              child: ListView.builder(
+                controller: controller,
+                itemCount: widget.messages.length,
+                shrinkWrap: true,
+                padding: const EdgeInsets.only(top: 20, bottom: 20),
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.only(
+                        left: 14, right: 14, top: 10, bottom: 10),
+                    child: Align(
+                      alignment:
+                          (widget.messages[index].senderId != widget.userId
+                              ? Alignment.topLeft
+                              : Alignment.topRight),
+                      child: Column(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: (widget.messages[index].senderId !=
+                                      widget.userId
                                   ? Colors.grey.shade200
                                   : Colors.blue[200]),
-                        ),
-                        padding: const EdgeInsets.all(16),
-                        child: Text(
-                          widget.messages[index].content,
-                          style: const TextStyle(fontSize: 15),
-                        ),
-                      ),
-                      Text(
-                        DateFormat('kk:mm dd/MM/yy').format(
-                            DateTime.parse(widget.messages[index].createdAt)
-                                .toLocal()),
-                        style: const TextStyle(fontSize: 11),
-                      ),
-                      Text(
-                        widget.messages[index].senderId.toString(),
-                        style: const TextStyle(fontSize: 11),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              padding: const EdgeInsets.only(
-                left: 10,
-                bottom: 10,
-                top: 10,
-              ),
-              height: 60,
-              width: double.infinity,
-              color: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  Expanded(
-                    child: TextField(
-                      cursorColor: darkBlue,
-                      decoration: const InputDecoration(
-                        hintText: "Vôtre message...",
-                        hintStyle: TextStyle(
-                          color: Colors.black54,
-                        ),
-                        border: InputBorder.none,
+                            ),
+                            padding: const EdgeInsets.all(16),
+                            child: Text(
+                              widget.messages[index].content,
+                              style: const TextStyle(fontSize: 15),
+                            ),
+                          ),
+                          Text(
+                            DateFormat('kk:mm dd/MM/yy').format(
+                                DateTime.parse(widget.messages[index].createdAt)
+                                    .toLocal()),
+                            style: const TextStyle(fontSize: 11),
+                          ),
+                          Text(
+                            widget.messages[index].senderId.toString(),
+                            style: const TextStyle(fontSize: 11),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {},
-                    child: const Icon(
-                      Icons.send,
-                      color: Colors.white,
-                      size: 18,
-                    ),
-                    backgroundColor: darkBlue,
-                    elevation: 0,
-                  ),
-                ],
+                  );
+                },
               ),
             ),
-          ),
-        ],
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                padding: const EdgeInsets.only(
+                  left: 10,
+                  bottom: 10,
+                  top: 10,
+                ),
+                height: 60,
+                width: double.infinity,
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Expanded(
+                      child: TextField(
+                        cursorColor: darkBlue,
+                        decoration: const InputDecoration(
+                          hintText: "Vôtre message...",
+                          hintStyle: TextStyle(
+                            color: Colors.black54,
+                          ),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    FloatingActionButton(
+                      onPressed: () {},
+                      child: const Icon(
+                        Icons.send,
+                        color: Colors.white,
+                        size: 18,
+                      ),
+                      backgroundColor: darkBlue,
+                      elevation: 0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
