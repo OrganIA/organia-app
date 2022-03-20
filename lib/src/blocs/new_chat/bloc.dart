ad481b5d1769788789470dfa06723e009be78674
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-03-21 02:19:43 +0900

    Add basic event handling

--- lib/src/blocs/new_chat/bloc.dart
+++ lib/src/blocs/new_chat/bloc.dart
@@ -1,30 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:organia/src/data/repository.dart';
-import 'package:organia/src/models/chat.dart';
-import 'package:organia/src/models/message.dart';
-import 'package:organia/src/utils/shared_preferences.dart';
 part 'event.dart';
 part 'state.dart';
 
 class NewChatBloc extends Bloc<NewChatEvent, NewChatState> {
-  final NewChatState initialState;
-  final Chat chat;
   final OrganIAAPIRepository organIAAPIRepository = OrganIAAPIRepository();
 
-  NewChatBloc(this.initialState, this.chat) : super(initialState) {
-    on<NewChatNavigateEvent>(
-        (event, emit) => emit(NewChatNavigate(chat, event.to)));
-    on<NewChatLoadEvent>((event, emit) async =>
-        emit(await _getNewChatMessages(event.chat.chatId)));
-    on<NewChatNavigationDoneEvent>((event, emit) => emit(NewChatLoading(chat)));
+  NewChatBloc() : super(const NewChatLoading()) {
+    on<NewChatNavigateEvent>((event, emit) => emit(const NewChatNavigate()));
+    on<NewChatLoadEvent>((event, emit) async => emit(const NewChatLoaded(1)));
+    on<NewChatNavigationDoneEvent>(
+        (event, emit) => emit(const NewChatLoading()));
   }
 
-  Future<NewChatState> _getNewChatMessages(int id) async {
-    String token = await MySharedPreferences().get("TOKEN");
-    List<Message> messagesList =
-        await organIAAPIRepository.getChatMessages(token, id);
-    return NewChatLoaded(messagesList, chat,
-        int.parse(await MySharedPreferences().get("USER_ID")));
-  }
+  // Future<NewChatState> _getNewChatMessages(int id) async {
+  //   String token = await MySharedPreferences().get("TOKEN");
+  //   List<Message> messagesList =
+  //       await organIAAPIRepository.getChatMessages(token, id);
+  //   return NewChatLoaded(messagesList, chat,
+  //       int.parse(await MySharedPreferences().get("USER_ID")));
+  // }
 }
