42ac418d943e420477681f0bd5c6e61fa239ae22
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-03-23 23:24:18 +0900

    Fix UI

--- lib/src/ui/widgets/chats_list/guest.dart
+++ lib/src/ui/widgets/chats_list/guest.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:organia/src/blocs/chats_list/bloc.dart';
 import 'package:organia/src/ui/themes/themes.dart';
-// import 'package:organia/src/ui/widgets/big_button.dart';
 
 class ChatsListGuestPage extends StatefulWidget {
   const ChatsListGuestPage({Key? key}) : super(key: key);
@@ -17,14 +14,14 @@ class _ChatsListGuestPageState extends State<ChatsListGuestPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Text(
-            "Vous n'êtes pas connecté.",
-            style: GoogleFonts.nunito(
-              textStyle: heading5,
-            ),
-          )),
+      child: Center(
+        child: Text(
+          "Vous n'êtes pas connecté.",
+          style: GoogleFonts.nunito(
+            textStyle: heading5,
+          ),
+        ),
+      ),
     );
   }
 }
