0b70958bab62a203141a616deacc04d4da2c0f54
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-03-24 19:23:12 +0900

    Add new chat UI

--- lib/src/ui/widgets/new_chat/loaded.dart
+++ lib/src/ui/widgets/new_chat/loaded.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:organia/src/ui/themes/themes.dart';
+import 'package:organia/src/ui/widgets/big_button.dart';
 
 class NewChatLoadedPage extends StatefulWidget {
   const NewChatLoadedPage({Key? key}) : super(key: key);
@@ -8,9 +11,173 @@ class NewChatLoadedPage extends StatefulWidget {
 }
 
 class _NewChatLoadedPageState extends State<NewChatLoadedPage> {
-  final ScrollController controller = ScrollController();
+  TextEditingController chatNameController = TextEditingController();
+  List<String> usersNotAdded = [];
+  List<String> usersToAdd = [];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          "Nouvelle Conversation",
+          style: GoogleFonts.nunito(
+            textStyle: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: black,
+            ),
+          ),
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15),
+              child: TextField(
+                controller: chatNameController,
+                decoration: InputDecoration(
+                  hintText: "Nom de la conversation",
+                  hintStyle: TextStyle(color: Colors.grey.shade600),
+                  filled: true,
+                  contentPadding: const EdgeInsets.all(15),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.grey.shade100),
+                  ),
+                ),
+                style: GoogleFonts.nunito(),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Utilisateurs ajoutés",
+                style: GoogleFonts.nunito(
+                  textStyle: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: black,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  childAspectRatio: 3,
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  children: usersToAdd.map<Widget>(
+                    (user) {
+                      return (InkWell(
+                        child: Card(
+                          color: darkBlue,
+                          elevation: 5,
+                          margin: const EdgeInsets.all(2.5),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(15),
+                              child: Text(
+                                user,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        onTap: () {
+                          setState(() {
+                            usersNotAdded.add(user);
+                            usersToAdd
+                                .removeWhere((element) => user == element);
+                          });
+                        },
+                      ));
+                    },
+                  ).toList(),
+                  shrinkWrap: true,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                "Utilisateurs non ajoutés",
+                style: GoogleFonts.nunito(
+                  textStyle: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: black,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        childAspectRatio: 3,
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        children: usersNotAdded.map<Widget>(
+                          (user) {
+                            return (InkWell(
+                              child: Card(
+                                color: darkBlue,
+                                elevation: 5,
+                                margin: const EdgeInsets.all(2.5),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                ),
+                                child: Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(15),
+                                    child: Text(
+                                      user,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  usersToAdd.add(user);
+                                  usersNotAdded.removeWhere(
+                                      (element) => user == element);
+                                });
+                              },
+                            ));
+                          },
+                        ).toList(),
+                        shrinkWrap: true,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.all(15),
+                child: BigButton(
+                  buttonColor: darkBlue,
+                  textColor: Colors.white,
+                  textValue: "Créer",
+                )),
+          ],
+        ),
+      ),
+    );
   }
 }
