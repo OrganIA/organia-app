31d44140911919cc106d694dd6b40d4cf0182de4
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-05-08 07:40:40 +0900

    Add red and light blue color

--- lib/src/ui/themes/themes.dart
+++ lib/src/ui/themes/themes.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
+Color lightBlue = const Color(0xFF90CAF9);
 Color blue = const Color(0xFF498BFE);
 Color darkBlue = const Color(0xFF071f49);
 Color black = const Color(0xff222222);
 Color grey = const Color(0xff8e8e8e);
+Color red = const Color(0xffff5b5b);
 
 TextStyle heading2 = const TextStyle(
   fontSize: 24,
