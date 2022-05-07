4c89c0133a5a3dbdef78024783b0c2cb96fb51f5
OkilSaber <okil-saber.lakhdari@epitech.eu>
2022-05-08 07:35:33 +0900

    Remove useless code

--- lib/src/ui/themes/themes.dart
+++ lib/src/ui/themes/themes.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 
-Color lightBlue = const Color(0xFF90CAF9);
 Color blue = const Color(0xFF498BFE);
 Color darkBlue = const Color(0xFF071f49);
 Color black = const Color(0xff222222);
 Color grey = const Color(0xff8e8e8e);
-Color lightGrey = const Color(0xFFEEEEEE);
-Color red = const Color(0xffff5b5b);
 
 TextStyle heading2 = const TextStyle(
   fontSize: 24,
@@ -21,9 +18,4 @@ TextStyle heading5 = const TextStyle(
 TextStyle heading6 = const TextStyle(
   fontSize: 16,
   fontWeight: FontWeight.w600,
-);
-
-TextStyle regular16pt = const TextStyle(
-  fontSize: 16,
-  fontWeight: FontWeight.w400,
 );
